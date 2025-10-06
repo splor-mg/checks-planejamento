30010f72bced1e40d32da261f11b2a1c329af9fb
labanca <moraislabanca@gmail.com>
2025-10-06 20:02:09 -0300

    fix(testes): usa quantidade de caracteres literal para evitar erro de evaluation.

--- R/check_indicadores_justificativa_indice_referencia_em_apuracao.R
+++ R/check_indicadores_justificativa_indice_referencia_em_apuracao.R
@@ -11,28 +11,28 @@ check_indicadores_justificativa_indice_referencia_em_apuracao <- function(indica
                                                                           json_outfile = NULL,
                                                                           log_level = "ERROR",
                                                                           msg_template = NULL
-                                                                          ) {
+) {
   df <- indicadores_planejamento
-  num_caracteres = 20
+  
   report <- check_that(df,
                        if (is_deleted_programa == FALSE &
                            is_deleted_indicador == FALSE & 
                            is_em_apuracao_indice_de_referencia == TRUE
-                           ) 
-                           !is.na(justificativa_status_apuracao_indice_ref) & 
-                           (stringr::str_length(justificativa_status_apuracao_indice_ref) > num_caracteres)
-                       )
+                       ) 
+                         !is.na(justificativa_status_apuracao_indice_ref) & 
+                         (stringr::str_length(justificativa_status_apuracao_indice_ref) > 20)
+  )
   
-  default_message = "O programa {programa_cod} contém indicador ({paste(unlist(strsplit(indicador, ' '))[1:3], collapse = ' ')}...) que está em apuração, porém com justificativa de status inválida, menos de {num_caracteres} caracteres {ifelse(is.na(justificativa_status_apuracao_indice_ref), '(0 caract.)', paste0('(', stringr::str_length(justificativa_status_apuracao_indice_ref), ' caract. - ', paste(unlist(strsplit(justificativa_status_apuracao_indice_ref, ' '))[1:3], collapse = ' '), '...)'))}."
+  default_message = "O programa {programa_cod} contém indicador ({paste(unlist(strsplit(indicador, ' '))[1:3], collapse = ' ')}...) que está em apuração, porém com justificativa de status inválida, menos de 20 caracteres {ifelse(is.na(justificativa_status_apuracao_indice_ref), '(0 caract.)', paste0('(', stringr::str_length(justificativa_status_apuracao_indice_ref), ' caract. - ', paste(unlist(strsplit(justificativa_status_apuracao_indice_ref, ' '))[1:3], collapse = ' '), '...)'))}."
   
   # prioritize the parameter error message if used
   msg_template = msg_template %||% default_message
-
+  
   check_result(df, report,
                stop_on_failure = stop_on_failure,
                output = output,
                json_outfile = json_outfile,
                log_level = log_level,
                msg_template = msg_template
-               )
+  )
 }
